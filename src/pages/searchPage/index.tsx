30c6b80968fed3fe9e7e62729b35289800d37c08
officeshinyujun <officeshinyujun@gmail.com>
2025-03-17 23:49:20 +0900

    add/search

--- src/pages/searchPage/index.tsx
+++ src/pages/searchPage/index.tsx
@@ -3,21 +3,21 @@ import Header from "../../components/header";
 import { IoMdSearch } from "react-icons/io";
 import haed from "../../assets/haed.jpg";
 import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import gsap from "gsap";
 
 export default function SearchPage(){
-
     const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState("");
 
     const list = [
         {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
-        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "asdf", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "qwer", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "zxcv", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "4567", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "erty", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "fgdh", explanation : "asdfasdf"},
+        {id : "haedam choi [spiderverse modular]", img : haed, link : "/haed", title : "cvbn", explanation : "asdfasdf"},
     ]
 
     useEffect(() => {
@@ -33,21 +33,26 @@ export default function SearchPage(){
                     <p>ARTWORK</p>
                 </section>
                 <section className={s.searchContainer}>
-                    <div className={s.inputContainer}>
-                        <input placeholder={"test.."}></input>
+                    <div className={s.inputContainer}><input placeholder={"Search..."} onChange={(e) => setSearchTerm(e.target.value)}></input>
                         <IoMdSearch size={30} className={s.searchIcon}/>
                     </div>
                     <button>ADD YOUR ARTWORK</button>
                 </section>
                 <section className={s.itemList}>
-                    {list.map((item) => (
-                        <div style={{ backgroundImage: `url(${item.img})`, backgroundSize: 'cover'}} className={s.itemContainer} onClick={() => navigate(`${item.link}`)}>
-                            <div className={s.informationSection}>
-                                <p>{item.title}</p>
-                                <span>{item.explanation}</span>
+                    {list
+                        .filter((item) => item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                        .map((item) => (
+                            <div
+                                style={{ backgroundImage: `url(${item.img})`, backgroundSize: "cover" }}
+                                className={s.itemContainer}
+                                onClick={() => navigate(`${item.link}`)}
+                            >
+                                <div className={s.informationSection}>
+                                    <p>{item.title}</p>
+                                    <span>{item.explanation}</span>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
                 </section>
             </div>
         </div>
